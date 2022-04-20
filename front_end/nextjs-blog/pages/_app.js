ad6dfa4221118070aeeca4e51751b292f0a94792
Y.A <y-azuma@morrin.co.jp>
2022-04-20 19:07:22 +0900

    add fetching, pre-rendering from tutorial

--- front_end/nextjs-blog/pages/_app.js
+++ front_end/nextjs-blog/pages/_app.js
@@ -1,5 +1,9 @@
 import '../styles/globals.css'
 
+/*
+  _app.js は特殊な意味を持ち、全てのコンポーネント、ページで読み込まれる。
+  そのため、コンポーネント単位ではなく、グローバルなCSSを適用したいのなら、この_app.jsで行う
+*/
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- front_end/nextjs-blog/pages/posts/first-post.js
+++ front_end/nextjs-blog/pages/posts/first-post.js
@@ -1,16 +1,40 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script'; // Facebook ソーシャルプラグインを追加してみる
 import styles from '../../styles/Home.module.css';
+import Layout from '/components/layout';
+{/*
+  解説サイトだと、from '../../components/layout' だったけど、現VerのNextはルートパスに対応している模様
+  なので、from '/components/layout' でいけてるようだ
+*/}
 
 export default function FirstPost () {
   return (
     <>
-      <p>this is first post</p>
+      <Layout>
+        <Head>
+          <title>postのindexページ</title>
+        </Head>
+        <div>
+          <p className={styles.title}>this is first post</p>
 
-      <h4 className={styles.title}>
-        <Link href="/">
-          <a>Back to Home</a>
-        </Link>
-      </h4>
+          {/* Third Party JavaScriptを使う場合、以下の様にstrategyで'lazyOnload'を指定する
+              参考：https://dev.classmethod.jp/articles/introduction-to-nextjs/#toc-5 */}
+          <script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() => 
+              console.log(`script loaded correctly, window.FB has been populated`)
+            }
+          />
+
+          <h4 className={styles.title}>
+            <Link href="/">
+              <a>Back to Home</a>
+            </Link>
+          </h4>
+        </div>
+      </Layout>
     </>
   );
 };
